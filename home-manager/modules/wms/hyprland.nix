aa23229aac15793f7e7f8d8fa727b46c5739d438
stringydev <107803920+stringydev@users.noreply.github>
2024-11-25 13:06:31 +0000

    fix(hyprland): added basic configs

--- home-manager/modules/wms/hyprland.nix
+++ home-manager/modules/wms/hyprland.nix
@@ -1,4 +1,24 @@
 {
   programs.kitty.enable = true; # required for the default Hyprland config
-  wayland.windowManager.hyprland.enable = true; # enable Hyprland
+  
+  wayland.windowManager.hyprland = {
+    enable =  true;
+    xwayland.enable = true;
+
+    settings = {
+      "mainMod" = "SUPER"
+
+      monitor = ",1920x1080@75,auto,1";
+
+      env = [
+        "XDG_CURRENT_DESKTOP,Hyprland"
+	"XDG_SESSION_TYPE,wayland"
+	"XDG_SESSION_DESKTOP,Hyprland"
+      ];
+
+      debug = {
+        disable_logs = false;
+	enable_stdout_logs = true;
+      };
+    };
 }
